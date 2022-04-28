d4d3d4fc2c38550f3fbeed06ad2c39354cfddff0
Andreas Baumann <mail@andreasbaumann.cc>
2022-04-28 15:13:55 +0000

    saner return values for rsvg_handle_get_intrinsic_dimensions and rsvg_handle_get_intrinsic_size_in_pixels, no fprintf

--- rsvg.c
+++ rsvg.c
@@ -127,13 +127,16 @@ void rsvg_handle_get_intrinsic_dimensions (RsvgHandle *handle,
                                            gboolean   *out_has_viewbox,
                                            RsvgRectangle *out_viewbox)
 {
-    fprintf( stderr, "Unimplemented function '%s' called!\n", "rsvg_handle_get_intrinsic_dimensions" );
+    *out_has_width = FALSE;
+    *out_has_height = FALSE;
+    *out_has_viewbox = FALSE;
 }
 
 gboolean rsvg_handle_get_intrinsic_size_in_pixels (RsvgHandle *handle,
                                                    gdouble    *out_width,
                                                    gdouble    *out_height)
 {
-    fprintf( stderr, "Unimplemented function '%s' called!\n", "rsvg_handle_get_intrinsic_size_in_pixels" );
+    *out_width = 0.0;
+    *out_height = 0.0;
     return FALSE;
 }
